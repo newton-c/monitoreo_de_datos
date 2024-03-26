9c64d3afe6bcf81f6e5a0dcd2fba5270744f7a8d
newton-c <newton.christop@gmail.com>
2024-03-26 15:10:33 -0300

    Fixed bug with x-axis labels being in the wrong place

--- scripts/ven_team.R
+++ scripts/ven_team.R
@@ -27,20 +27,24 @@ pv_ven <- southam_data |>
 all_ven_pv_filename <- paste0("ven/", "Ven_all_pv_events_", plot_month, ".csv")
 write_excel_csv(pv_ven, all_ven_pv_filename)
 
+pv_ven <- pv_ven |>
+  mutate(event = 1)
+
 # Monthly event types
 pv_ven_event_type <- pv_ven |>
   filter(event_type %in% ven_events_list) |>
   mutate(month = months(as.Date(event_date))) |>
   group_by(month, year, event_type) |>
   summarise(events = sum(event)) |>
-  mutate(year_mon = as.yearmon(paste(month, year, sep = " "), format = "%B %Y"))
+  mutate(year_mon = as.yearmon(paste(month, year, "01", sep = " "),
+                               format = "%B %Y %d")) 
 
 monthly_ven_events_filename <- paste0("ven/", "Ven_monthly_by_event_type_",
                                       plot_month, ".csv")
 write_excel_csv(pv_ven_event_type, monthly_ven_events_filename)
 
 ven_event_plot <- ggplot() +
-  geom_line(pv_ven_event_type,
+  geom_line(pv_ven_event_type, 
             mapping = aes(x = year_mon, y = events), linewidth = 2,
             color = "#B20D2B", group = 1) +
   geom_point(pv_ven_event_type,
@@ -68,10 +72,10 @@ ven_event_plot <- ggplot() +
         plot.caption = element_text(family = "Roboto", size = 11,
                                     color = "#b3b3b3", hjust = 0),
         plot.caption.position = "plot") +
-  facet_wrap(facets = ~event_type, scales = "free_y", nrow = 2)
+  facet_wrap(facets = ~event_type, scales = "free", nrow = 2)
 ven_event_plot
 
-# Monthly event types
+# Monthly sub-event types
 ven_sub_events_list <- c("Abduction/forced disappearance", "Armed clash",
                          "Arrests", "Attack", "Change to group/activity",
                          "Disrupted weapons use", "Grenade",
@@ -84,7 +88,8 @@ pv_ven_sub_event_type <- pv_ven |>
   mutate(month = months(as.Date(event_date))) |>
   group_by(month, year, sub_event_type) |>
   summarise(events = sum(event)) |>
-  mutate(year_mon = as.yearmon(paste(month, year, sep = " "), format = "%B %Y"))
+  mutate(year_mon = as.yearmon(paste(month, year, "01", sep = " "),
+                               format = "%B %Y %d"))
 
 monthly_ven_sub_events_filename <- paste0("ven/",
                                           "Ven_monthly_by_sub_event_type_",
