0e09c7507ea3df784e4d610a97a68d14e61ce4b8
newton-c <newton.christop@gmail.com>
2024-04-17 18:05:44 -0300

    add catch for uninstalled packages

--- principal.R
+++ principal.R
@@ -1,3 +1,10 @@
+list.of.packages <- c("tidyverse", "ICplots", "lubridate", "aweek", "httr",
+                      "jsonlite", "readr", "zoo", "gt", "sf")
+new.packages <- list.of.packages[!(list.of.packages %in%
+                                     installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+rm(list = c("list.of.packages", "new.packages"))
+
 library(tidyverse)
 library(ICplots)
 library(lubridate)
@@ -16,7 +23,7 @@ max_date <- Sys.Date() - wday(Sys.Date() + 1)
 plot_month <- as.character(format((Sys.Date()), "%B %Y"))
 
 source("scripts/acled.R")
-source("scripts/ven_team.R")
+#source("scripts/ven_team.R")
 source("scripts/col_team.R")
 source("scripts/mexico_elections.R")
 #source("scripts/acled_cleaning.R")
